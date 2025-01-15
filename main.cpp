8f5d763cb22d74444ef731f01f1628363164f480
VictorJarquin <equipo.alebrijes2013@gmail.com>
2025-01-14 22:57:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "mbed.h"
 
+DigitalOut myled(PC_13);
+
 int main()
 {
-    DigitalIn B1_USER(BUTTON1);
-
-    DigitalOut LD1(LED1);
-
-    while (true) {
-        LD1 = B1_USER;
-    }
+    while (true) 
+    {
+        myled = 1;
+        wait(0.2);
+        myled = 0;
+        wait(1);
+    }
 }
